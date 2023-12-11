dac30c407011310b97329ae6b59437633bf4f88f
elotoja <lukasz.budziak@masters.com.pl>
2023-12-11 14:46:20 +0100

    Add Moq to global usings

---
+++ tests/Auth.Services.Tests/Implementations/AuthServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Auth.Services.Implementation;
+using Auth.Services.Interface;
+using Microsoft.Extensions.Configuration;
+
+namespace Auth.Services.Tests.Implementations
+{
+    internal class AuthServiceTests
+    {
+        private readonly IAuthService _authService;
+
+        public AuthServiceTests()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            _authService = new AuthService(configuration);
+        }
+    }
+}
